227561b269838c3b4321839c621bca5f2b569981
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-17 17:30:22 +0530

    Create 24.C

---
+++ 24.C
@@ -0,0 +1,34 @@
+int main()
+{
+	int a,b[100],c,d,i,j,h=0,t;
+	scanf("%d",&a); printf("%d",a);
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&b[i]); //printf("%d\t",b[i]);
+	}
+	for(j=h;j<a;j++)
+		{
+			c=b[h];
+			for(i=j+1;i<a;i++)
+			{
+				
+			if(c>b[i])
+			{
+			c=b[i];	printf("%d\n",c);
+				d=i;	
+				}
+			}
+			if(c<b[h])
+			{
+			t=b[d];
+			b[d]=b[h];
+			b[h]=t;
+				}
+			h++;
+			}
+			for(i=0;i<a;i++)
+			{
+			
+	printf("%d\t",b[i]);
+			}
+			}
