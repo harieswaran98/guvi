2482227fe959f4714396f564f2a1665051d09014
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-19 15:35:46 +0530

    Create 57.C

---
+++ 57.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+   int a,b,d=0,e=0,c[100],i,j;
+   scanf("%d %d",&a,&b);
+   for(i=0;i<a;i++)
+   {
+       scanf("%d",&c[i]);
+       if(c[i]==b)
+       d++;
+   }
+   printf("%d",d);
+   
+  
+}
