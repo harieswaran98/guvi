5bd97e73fb340f09c764deea9dad6b2c834f164b
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-20 20:00:58 +0530

    Create 68.C

---
+++ 68.C
@@ -0,0 +1,21 @@
+int main()
+{
+	int a,b=7,c=1,e=0;
+	scanf("%d",&a);
+	while(b<=a)
+	{
+		b=7;
+
+	b=b*c; // printf("%d",b);
+    if(a==b)
+	{
+		e=1;
+		break;
+		}
+		c++;
+		}
+		if(e==1)
+		printf("yes");
+		else
+		printf("no");
+		}
