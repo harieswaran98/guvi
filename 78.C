ef71f589cc43c1c43270caafa66b67f5268f38db
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-20 21:45:15 +0530

    Create 78.C

---
+++ 78.C
@@ -0,0 +1,23 @@
+int main()
+{
+	int a,b=13,c=1,d=0;
+	scanf("%d",&a);
+while(c)
+	{
+		b=13;
+		b=b*c;
+		c++;
+		if(b==a)
+		{
+		d=1;
+		break;
+			}
+		if(b>a)
+			break;
+			}
+		
+		if(d==1)
+		printf("yes");
+		else
+		printf("no");
+		}
