f1000edb13f389d63688d710f9902c884104ba19
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-21 16:21:41 +0530

    Create 87.C

---
+++ 87.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+   int x,b,i,j,y,z;
+   scanf("%d %d",&x,&b);
+  // printf("%d %d ",x,b);
+   if(x>b)
+   {
+       y=x;
+       x=b;
+       b=y;
+       
+   }
+  z=x;   //printf("%d ",z);
+  while(z)
+  {
+   if((x%z==0)&&(b%z==0))
+   {
+   printf("%d",z);
+   z=0;
+   }
+   else
+   z--; // printf("%d ",z);
+  }
+   
+  
+}
