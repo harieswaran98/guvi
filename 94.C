f88ea27d31c2f6f42099179f332f3e4b26bd27e2
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-21 20:24:47 +0530

    Create 94.C

---
+++ 94.C
@@ -0,0 +1,15 @@
+int main()
+{
+	int a,b=0,c,d,e[100],i;
+	scanf("%d %d",&a,&b);
+	scanf("\n");
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&e[i]);
+		}
+		for(i=0;i<a;i++)
+		{
+		if(i==b-1)
+         printf("%d",e[i]);
+	}
+	}
