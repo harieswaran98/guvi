ceaa79849ef9075df1ad2fdbc3ef9be3f1116232
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-22 09:58:45 +0530

    Update 103.C

--- 103.C
+++ 103.C
@@ -3,11 +3,19 @@
 int main() {
    int a,b,c,i;
    char d[100];
-   scanf("%[^\n]s",&d);
+   gets(d);
    for(i=0;d[i]!='\0';i++)
    {
-       if((i=0)||(d[i]==' '))
-       d[i]=d[i]+32;
+       if(d[i]==' ')
+       {
+           if((d[i+1]>=97)&&(d[i+1]<=122))
+              d[i+1]=d[i+1]-32;
+       }
+       if(i==0)
+       {
+        if((d[i]>=97)&&(d[i]<=122))
+       d[i]=d[i]-32;
+       }
    }
    for(i=0;d[i]!='\0';i++)
    printf("%c",d[i]);
